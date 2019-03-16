1f916fe66bc04bdc20f2d713a86c8158e0a5961b
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2019-03-16 19:42:34 +0200

    Lisätty tiedosto josta voi muokata globaaleja muuttujia

---
+++ src/globals.py
@@ -0,0 +1,15 @@
+
+
+
+def init():
+    global SCREEN_WIDTH
+    global SCREEN_HEIGHT
+    global PLAYER_SPEED
+    global FRAME_SPEED
+    global GRAVITY
+
+    SCREEN_WIDTH  = 800
+    SCREEN_HEIGHT = 600
+    PLAYER_SPEED  = 3
+    FRAME_SPEED   = 17
+    GRAVITY       = 0.6
