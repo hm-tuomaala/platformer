bbdb60b38e0dfa53d3fc3a29bf7b6d3f1627800e
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2019-03-20 17:06:16 +0200

    Info näkymän rakentelua

--- src/infoscene.py
+++ src/infoscene.py
@@ -17,18 +17,44 @@ class Infoscene(QGraphicsScene):
         bg.setBrush(QBrush(Qt.black))
         self.addItem(bg)
 
-        self.button1 = QGraphicsRectItem(147, 300, 104, 60)
+        self.button1 = QGraphicsRectItem(40, 500, 110, 60)
         self.button1.setBrush(QBrush(QtGui.QColor(38, 38, 38)))
         self.addItem(self.button1)
 
+        self.buttonFont = QtGui.QFont()
+        self.buttonFont.setPointSize(20)
+        self.textFont = QtGui.QFont()
+        self.textFont.setPointSize(18)
+        self.titleFont = QtGui.QFont()
+        self.titleFont.setPointSize(30)
+
         self.b1 = QtWidgets.QGraphicsTextItem('BACK')
         self.b1.setDefaultTextColor(QtGui.QColor(255, 0, 0))
-        self.b1.setPos(150, 300)
+        self.b1.setPos(43, 500)
+        self.b1.setFont(self.buttonFont)
         self.addItem(self.b1)
 
+        self.title = QtWidgets.QGraphicsTextItem('How To Play')
+        self.title.setDefaultTextColor(QtGui.QColor(255, 0, 0))
+        self.title.setPos(200, 25)
+        self.title.setFont(self.titleFont)
+        self.addItem(self.title)
+
+        text = ('The player can move with left and right arrow keys and jump with space. '
+                + 'Points are awarded by collecting small red awards but be aware of '
+                + 'green enemys: If you touch them you die! If you can get to the '
+                + 'goal you win the level!')
+        self.text = QtWidgets.QGraphicsTextItem(text)
+        self.text.setDefaultTextColor(QtGui.QColor(255, 255, 255))
+        self.text.setPos(150, 130)
+        self.text.setFont(self.textFont)
+        self.text.adjustSize()
+        self.addItem(self.text)
+
         self.view.update_scene(self)
+        #self.view.ensureVisible(bg)
 
     def mousePressEvent(self, QMouseEvent):
-        if(QMouseEvent.scenePos().x() >= 147 and QMouseEvent.scenePos().x() <= 147+104
-            and QMouseEvent.scenePos().y() >= 300 and QMouseEvent.scenePos().y() <= 300+60):
+        if(QMouseEvent.scenePos().x() >= 40 and QMouseEvent.scenePos().x() <= 40+110
+            and QMouseEvent.scenePos().y() >= 500 and QMouseEvent.scenePos().y() <= 500+60):
             self.view.update_scene(self.menu)
